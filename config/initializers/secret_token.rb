e2aa50d797b9492a82df43d1c1fa3412d157f4c5
Michał Chołka <mcholka@gmail.com>
2012-01-12 20:02:37 +0100

    cos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testty::Application.config.secret_token = '353346230c19c7f3e7beabe4e745e319932017a6c7f6d89e61c57ea6ad63e2c1a85cd3d39df953ab99359b138ffa2b7132453360b7a8f51080b60b0d437bec9f'
